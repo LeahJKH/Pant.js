c0ce35cc9bb78a2ca92515a89597847b806ea999
davidKH <davidjb06.kodehode@gmail.com>
2023-11-23 14:37:39 +0100

    dragged people

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import { peopleArray } from "./module";
-import { Elements } from "./module";
+import { peopleArray } from "./module.js";
+import { Elements } from "./module.js";
 
+document.addEventListener("DOMContentLoaded", (e) => {
+  function GetChecklist() {
+    for (let i = 0; i < peopleArray.length; i++) {
+      Elements.peopleCheckList.innerHTML += ` <input type="checkbox" name="peoplePresent" /><label for="peoplePresent">${peopleArray[i]}</label>`;
+    }
+  }
 
-function randomizer 
+  function randomizer() {}
+  GetChecklist();
+});
+
+//note to kai:
+
+make a
